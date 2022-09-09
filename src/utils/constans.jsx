81b41b79bb88c7ce27555610cb3e7dffbb1caeab
jersonsilvaa <jerson.silva99@hotmail.com>
2022-09-08 21:37:43 -0500

    fix: Fixed, the error was because i trying use .js and for fix i hade change .js for .jsx

---
+++ src/utils/constans.jsx
@@ -0,0 +1,49 @@
+import {
+    FaBars,
+    FaHome,
+    FaCode,
+    FaMusic,
+    FaPencilRuler,
+    FaPodcast,
+    FaVuejs,
+    FaReact,
+    FaVideo,
+    FaBasketballBall,
+    FaTshirt,
+    FaWeightHanging,
+    FaSmileBeam,
+    FaDumbbell,
+    FaBitcoin,
+    FaGamepad,
+    FaChromecast,
+    FaSearch,
+  } from "react-icons/fa";
+
+export const logo = '../assets/yt-image.png'
+
+export const categories = [
+    { name: 'Nuevo', icon: < FaHome /> },
+    { name: 'Midudev', icon: <FaCode /> },
+    { name: 'Programación', icon: <FaCode /> },
+    { name: 'ReactJS', icon: <FaReact /> },
+    { name: 'Vue.js', icon: <FaVuejs /> },
+    { name: 'Música', icon: <FaMusic /> },
+    { name: 'Educación', icon: <FaPencilRuler /> },
+    { name: 'Podcast', icon: <FaPodcast /> },
+    { name: 'Película', icon: <FaVideo /> },
+    { name: 'Gaming', icon: <FaGamepad /> },
+    { name: 'Directo', icon: <FaChromecast /> },
+    { name: 'Deporte', icon: <FaBasketballBall /> },
+    { name: 'Moda', icon: <FaTshirt /> },
+    { name: 'Comedia', icon: <FaSmileBeam /> },
+    { name: 'Gym', icon: <FaWeightHanging /> },
+    { name: 'Bitcoin', icon:  <FaBitcoin /> }
+]
+
+//Demo of the components video
+
+export const demoThumbUrl = ''
+export const demoChannelUrl = ''
+export const demoVideoUrl = ''
+export const demoChannelTitle = 'Midudev'
+export const demoVideoTitle = 'Hacemos un clon funcional de TIKTOK'
