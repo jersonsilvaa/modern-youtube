c0dc2329dfa95307e628e721e3b9056f5498b8e7
jersonsilvaa <jerson.silva99@hotmail.com>
2022-09-08 21:32:06 -0500

    feat: Add sidebar component and import categories icons

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { categories } from '../utils/constans'
+
+import '../styles/components/SideBar.css'
+
+const SideBar = () => (
+    <div className="sidebar">
+    <section className="sidebar__section">
+        { categories.map((category) => (
+            <button className='button__category'>
+                <span>{ category.icon }</span>
+                <span>{ category.name }</span>
+            </button>
+        )) }
+    </section>
+    <p>Todos los derechos reservados © DevSilva</p>
+</div>
+)
+
+export default SideBar
